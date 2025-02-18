a4d34e3be7375f8d54af58c9de40fc0153f98f01
erica-monacillo <ericamonacillo09@gmail.com>
2025-02-18 22:01:11 +0800

    SignUp
    Update ui

--- src/components/AestheticShop.tsx
+++ src/components/AestheticShop.tsx
@@ -21,9 +21,9 @@ const AestheticShop: React.FC = () => {
               <HeroSection />
               <Categories />
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-                <ProductCard imageSrc="https://placehold.co/300x200" productName="Product 1" price="$99.99" />
-                <ProductCard imageSrc="https://placehold.co/300x200" productName="Product 2" price="$79.99" />
-                <ProductCard imageSrc="https://placehold.co/300x200" productName="Product 3" price="$49.99" />
+                <ProductCard imageSrc="https://cdn.luxe.digital/media/20240625085857/most-expensive-sneakers-glass-shard-air-jordan-1-luxe-digital-780x520.jpg" productName="Product 1" price="$99.99" />
+                <ProductCard imageSrc="https://visor.ph/wp-content/uploads/2018/06/suzuki-jimny-official-pics-main6.jpg" productName="Suzuki Jimny" price="$79.99" />
+                <ProductCard imageSrc="https://media.tenor.com/_zWYqfZdneIAAAAe/shocked-face-shocked-meme.png" productName="ADETANG" price="$49.99" />
               </div>
               <Footer />
             </>
